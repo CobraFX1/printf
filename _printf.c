3bab341e6d8065762f31d4ec713d8eb637de52f6
Jacob Stephen <cashflow34steph@gmail.com>
2025-04-22 10:02:43 -0700

    creating a fully functional printf function

--- _printf.c
+++ _printf.c
@@ -1,7 +1,13 @@
 #include "main.h"
 int _putchar(char c);
 int print_string(char *str);
-int print_num(int n);
+
+/**
+ * _printf - prints a format string
+ * @format: parameter
+ *
+ * Return: count of characters
+ */
 
 int _printf(const char *format, ...)
 {
@@ -20,44 +26,61 @@ int _printf(const char *format, ...)
 		if (format[i] == '%' && format[i + 1])
 		{
 			switch (format[i + 1])
-		{
-			case 'c':
-				count += _putchar(va_arg(args, int));
-				break;
-			case 's':
-			    count += print_string(va_arg(args, char *));
-			    break;
-			case 'd':
-			case 'i':
-				count += print_num(va_arg(args, int));
-		    case '%':
-			    count += _putchar('%');
-			    break;
-		    default:
-			    count += _putchar('%');
-			    count += _putchar(format[i + 1]);
-			    break;
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += print_string(va_arg(args, char *));
+					break;
+				case 'd':
+					count += print_num(va_arg(args, int));
+					break;
+				case 'i':
+					count += print_num(va_arg(args, int));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(format[i + 1]);
+					break;
+			}
+			i += 2;
 		}
-		    i += 2;
-	    }
-	    else if (format[i] == '%' && !format[i + 1])
-	    {
-		    return (-1);
-	    }
-	    else
-	    {
-		count += _putchar(format[i]);
-		i++;
-	    }
+		else if (format[i] == '%' && !format[i + 1])
+		{
+			return (-1);
+		}
+		else
+		{
+			count += _putchar(format[i]);
+			i++;
+		}
 	}
 	va_end(args);
 	return (count);
 }
 
+/**
+ * _putchar - prints a character
+ * @c: character
+ *
+ * Return: amount of characters printed
+ */
+
 int _putchar(char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
+
+/**
+ * print_string - prints a string of characters
+ * @str: parameter
+ *
+ * Return: amount of characters printed
+ */
 
 int print_string(char *str)
 {
@@ -66,37 +89,12 @@ int print_string(char *str)
 
 	if (!str)
 	{
-	    str = "(null)";
+		str = "(null)";
 	}
 	while (str[i])
 	{
-	    count += _putchar(str[i]);
-	    i++;
+		count += _putchar(str[i]);
+		i++;
 	}
 	return (count);
-}
-int print_num(int n)
-{
-    int count = 0;
-    int num;
-
-    if (n < 0)
-    {
-        count += _putchar('-');
-        num = -n;
-    }
-    else if (n >= 0)
-    {
-        num = n;
-    }
-    if (num / 10)
-    {
-        count += print_num(num / 10);
-        count += _putchar((num % 10) + '0');
-    }
-    else {
-        count += _putchar(num + '0');
-        return (count);
-    }
-    return (count);
 }
